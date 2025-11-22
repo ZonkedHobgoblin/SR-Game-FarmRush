188ab2ea7b859ba545be58b7b9884ff65ac0c06d
Stephen Robbins <stephen.robbins175@gmail.com>
2025-11-22 01:30:42 +0000

    Fixed animal sizes

--- Assets/Scripts/Unit2/pc2.cs
+++ Assets/Scripts/Unit2/pc2.cs
@@ -15,10 +15,21 @@ public class pc2 : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float Range = 10;
+    public GameObject projectile;
     void Update()
     {
+        //Movement stuff VVVVVVVVVVVVVVVVV
+        //input and movement
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-        transform.Clam
+        //clamp x axis because using if statements for this is stupid
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -10f, 10f), transform.position.y, transform.position.z);
+        
+        //projectile stuff VVVVVVVVVVVV
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //spawn projectile at the guy
+            Instantiate(projectile, transform.position, projectile.transform.rotation);
+        }
     }
 }
