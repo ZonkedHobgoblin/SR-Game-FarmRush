9009d1bce740d014406cdf576a805fc712b9129c
Stephen Robbins <stephen.robbins175@gmail.com>
2025-11-22 17:34:52 +0000

    working on animal script

--- Assets/Scripts/Unit2/projectile.cs
+++ Assets/Scripts/Unit2/projectile.cs
@@ -7,22 +7,31 @@ using UnityEngine;
 public class projectile : MonoBehaviour
 {
     //vars
-    public float speed = 40.0f;
+    public float[] speed;
     private float bound = 30;
     public Mesh[] model;
+    public string[] anims;
+    public bool isAnimal;
+    private float animalSpeed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
         //randomize and model so no need for multiple prefabs
         int ranNum = Random.Range(0, model.Length);
         GetComponent<MeshFilter>().mesh = model[ranNum];
-        // to do: fix size
+        //set speed from speed array
+        animalSpeed = speed[ranNum];
+        //check if its an animal, if yes then play anim from array (same script used for projectile so)
+        if (isAnimal == true)
+        {
+            GetComponent<Animator>().Play(anims[ranNum]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * animalSpeed);
         //delete the object when too far
         if (transform.position.z > bound || transform.position.z < -(bound - 20))
         {
