3c522163d3dd35bc2b7b4a6b14cfbc7521ec5645
STEPHEN ROBBINS <stephen.robbins@yorksj.ac.uk>
2025-11-28 10:39:54 +0000

    ui stuff

--- Assets/Scripts/Unit2/ui.cs
+++ Assets/Scripts/Unit2/ui.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class ui : MonoBehaviour
 {
     public int score;
-    public int health;
+    public float health;
     public GameObject gameovertext;
     public GameObject gameoverpanel;
-    public TextMeshProUGUI healthtext;
+    public Slider healthslider;
     public TextMeshProUGUI scoretext;
     // Start is called before the first frame update
     void Start()
@@ -18,8 +20,8 @@ public class ui : MonoBehaviour
     }
     private void Update()
     {
-        healthtext.text = "Health:" + health;
-        scoretext.text = "Score:" + score;
+        healthslider.value = health / 10.0f;
+        scoretext.text = "Score:\n" + score;
         if (health <= 0)
         {
             Gameover();
